1c9fc4ea26cdf64e872d849c91e68c01b60c7058
BudiNverse <me@inve.rs>
2021-12-11 23:16:30 +0800

    More ergonomic rust

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,10 @@ use detour::static_detour;
 use ntapi::ntpsapi::{ThreadHideFromDebugger, THREADINFOCLASS};
 use std::ffi::{c_void, CString};
 use std::os::raw::c_long;
+use std::ptr::NonNull;
 use std::time::Duration;
 use std::{mem, thread};
 use winapi::shared::minwindef::ULONG;
-use winapi::um::debugapi::IsDebuggerPresent;
 use winapi::um::libloaderapi::{GetModuleHandleA, GetProcAddress};
 use winapi::um::winnt::{HANDLE, PVOID};
 use winapi::{
@@ -33,18 +33,26 @@ type NtSetInformationThreadFn =
 
 #[dllmain_rs::entry()]
 fn real_entry() {
-    unsafe {
-        consoleapi::AllocConsole();
-    }
-
+    unsafe { consoleapi::AllocConsole() };
     thread::spawn(|| {
-        hook_NtSetInformationThread();
+        let dur = Duration::from_millis(500);
+        let mut hook_res = hook_NtSetInformationThread();
+        while let Err(e) = hook_res {
+            // sleep for awhile then retry
+            thread::sleep(dur);
+            hook_res = hook_NtSetInformationThread();
+        }
+
+        // dont let thread exit
+        // if the code reaches here, this means that the hook was successful
+        println!("[+] NtSetInformationThread hooked and enabled successfully!");
         loop {
-            thread::sleep(Duration::from_secs(1));
+            thread::sleep(dur);
         }
     });
 }
 
+#[allow(non_snake_case)]
 fn log_NtSetInformationThread(
     thread_handle: HANDLE,
     thread_info_class: THREADINFOCLASS,
@@ -65,54 +73,45 @@ fn log_NtSetInformationThread(
     // can also try to check for current module address
     // and if it is not called from the current module address range
     // then don't do anything
-    // if thread_info_class == ThreadHideFromDebugger {
-    //     println!("[+] ThreadHideFromDebugger detected! Undoing flag!");
-    //     0
-    // } else {
-    //     unsafe {
-    //         NtSetInformationThreadFnHook.call(
-    //             thread_handle,
-    //             thread_info_class,
-    //             thread_info,
-    //             thread_info_length,
-    //         )
-    //     }
-    // }
-
-    unsafe {
-        NtSetInformationThreadFnHook.call(
-            thread_handle,
-            thread_info_class,
-            thread_info,
-            thread_info_length,
-        )
-    }
-}
-
-fn hook_NtSetInformationThread() {
-    let addr = get_addr_exported_fn::<_, c_void>("ntdll.dll", "NtSetInformationThread");
-    if !addr.is_null() {
-        let addr = unsafe { mem::transmute::<_, NtSetInformationThreadFn>(addr) };
-
-        unsafe {
-            NtSetInformationThreadFnHook
-                .initialize(addr, log_NtSetInformationThread)
-                .unwrap()
-                .enable()
-                .unwrap();
-        }
+    if thread_info_class == ThreadHideFromDebugger {
+        println!("[+] ThreadHideFromDebugger detected! Undoing flag!");
+        0
     } else {
-        println!("[-] address is null!");
+        unsafe {
+            NtSetInformationThreadFnHook.call(
+                thread_handle,
+                thread_info_class,
+                thread_info,
+                thread_info_length,
+            )
+        }
     }
 }
 
-fn get_addr_exported_fn<S: AsRef<str>, T>(module: S, symbol: S) -> *const T {
+#[allow(non_snake_case)]
+fn hook_NtSetInformationThread() -> Result<(), &'static str> {
+    let addr = get_addr_exported_fn::<_, c_void>("ntdll.dll", "NtSetInformationThread");
+    let addr = addr.ok_or("Null pointer!")?;
+    let target = unsafe { mem::transmute::<_, NtSetInformationThreadFn>(addr) };
     unsafe {
-        let module = module.as_ref();
-        let symbol = symbol.as_ref();
-        let name = CString::new(module).unwrap();
+        NtSetInformationThreadFnHook
+            .initialize(target, log_NtSetInformationThread)
+            .map_err(|_| "Failed to initialise!")?
+            .enable()
+            .map_err(|_| "Failed to enable!")?;
+    }
+
+    Ok(())
+}
+
+
+/// Returns the address of the exported function
+fn get_addr_exported_fn<S: AsRef<str>, T>(module: S, symbol: S) -> Option<NonNull<T>> {
+    unsafe {
+        let name = CString::new(module.as_ref()).unwrap();
         let handle = GetModuleHandleA(name.as_ptr());
-        let symbol_name = CString::new(symbol).unwrap();
-        GetProcAddress(handle, symbol_name.as_ptr()) as *const T
+        let symbol_name = CString::new(symbol.as_ref()).unwrap();
+        let ptr = GetProcAddress(handle, symbol_name.as_ptr()) as *mut T;
+        NonNull::new(ptr)
     }
 }
